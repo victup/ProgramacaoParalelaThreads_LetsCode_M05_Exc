afc8ce8cad39ad04b587ecc3e536441e5dd99710
victup <victor.gsantos@hotmail.com>
2022-08-26 15:47:35 -0300

    Add project files.

---
+++ Exemplo1/Program.cs
@@ -0,0 +1,64 @@
+﻿
+
+using System.Diagnostics;
+using System.Threading;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        //Stopwatch stopwatch = new Stopwatch();
+
+        //stopwatch.Start();
+
+        //List<Thread> threads = new List<Thread>()
+        //{
+        //    new Thread(LavarLouca),
+        //    new Thread(LavarRoupa),
+        //    new Thread(FazerComida)
+        //};
+
+        //foreach (var thread in threads)
+        //{
+        //    thread.Start();
+        //}
+
+        //foreach (var thread in threads)
+        //{
+        //    thread?.Join();
+        //}
+
+        new Thread(LavarLouca).Start();
+        new Thread(LavarRoupa).Start();
+        new Thread(FazerComida).Start();
+
+
+        //Console.WriteLine($"O processo levou {stopwatch.ElapsedMilliseconds} ms");
+
+        //stopwatch.Stop();
+
+    }
+
+    static void LavarLouca()
+    {
+        Console.WriteLine($"Inicio Lavar louça. {Thread.CurrentThread.ManagedThreadId}");
+        Thread.Sleep(2000);
+        Console.WriteLine("Fim Lavar a Louça");
+    }
+
+    static void LavarRoupa()
+    {
+        Console.WriteLine($"Inicio Lavar a roupa. {Thread.CurrentThread.ManagedThreadId}");
+        Thread.Sleep(3000);
+        Console.WriteLine("Fim Lavar a roupa");
+    }
+
+    static void FazerComida()
+    {
+        Console.WriteLine($"Inicio Fazer comida. {Thread.CurrentThread.ManagedThreadId}");
+        Thread.Sleep(8000);
+        Console.WriteLine("Fim Fazer comida");
+    }
+}
---
+++ Exemplo2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        //List<string> files = new List<string>();
+
+        //files.Add("vs.exe");
+        //files.Add("note.exe");
+        //files.Add("calc.exe");
+        //files.Add("word.exe");
+
+        Stopwatch stopwatch = new Stopwatch();
+
+
+
+        stopwatch.Start();
+
+        ParallelOptions op = new();
+        op.MaxDegreeOfParallelism = 2; //maximo de threads que vai usar, respeitando a quantidade disponivel do sistema
+
+        //Parallel.For(0, files.Count - 1, op, i =>
+        //{
+        //    DownloadFile(files[i]);
+        //});
+
+        //Parallel.ForEach(files, op, file =>
+        //{
+        //    DownloadFile(file);
+        //});
+
+        Parallel.Invoke(op, () => DownloadFile("chrome.exe"),
+                        () => DownloadFile("opera.exe"),
+                        () => DownloadFile("firefox.exe"),
+                        () => DownloadFile("gx.exe")
+        );
+
+        stopwatch.Stop();
+        Console.WriteLine($"O processo levou {stopwatch.ElapsedMilliseconds} ms");
+
+    }
+
+
+    public static void DownloadFile(string file)
+    {
+        Thread.Sleep(5000);
+        Console.WriteLine($"Download Done: {file} - Thread ID: {Thread.CurrentThread.ManagedThreadId}");
+    }
+}
